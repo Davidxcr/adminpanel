00cc9630da467e72a72a7a0741d347e9c8c8ee8a
Davidxcr <vodolatunji@gmail.com>
2022-07-14 16:13:40 -0500

    working on single page for user info

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,29 @@
 import "./single.scss"
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
 
 const Single = () => {
     return (
         <div className='single'>
-            Single
+            <Sidebar />
+            <div className="singleContainer">
+                <Navbar />
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Esit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img
+                                src=" https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                                alt=""
+                                className="itemImg" />
+                            Details
+                        </div>
+                    </div>
+                    <div className="right"></div>
+                </div>
+                <div className="bottom"></div>
+            </div>
         </div>
     )
 }
